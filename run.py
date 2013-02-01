a8ec1238a0d25163993d0bd00cbb83ff8aeff52d
Pascal Pfiffner <phase.of.matter@gmail.com>
2013-02-01 18:33:40 -0500

    Now a background app, remove our token storage wizardry

--- run.py
+++ run.py
@@ -20,14 +20,12 @@ from umls import UMLS
 
 # sandbox default setting for testing purposes
 APP_ID = 'ae-reporting@apps.chip.org'
-#API_BASE = 'http://coruscant.local:7000'
-API_BASE = 'http://sandbox-rest.smartplatforms.org:7000'
+API_BASE = 'http://127.0.0.1:7000'
+#API_BASE = 'http://sandbox-rest.smartplatforms.org:7000'
 OAUTH_PARAMS = {
 	'consumer_key': 'ae-reporting@apps.chip.org',
-	'consumer_secret': 'fNpKMtZEhnhLGJqO'
+	'consumer_secret': 'NUzwBOMCPPxhfUQl'
 }
-
-KNOWN_TOKENS = {}
 
 
 def load_rules():
@@ -61,42 +59,15 @@ if __name__ == "__main__":
 		print forever_alone()
 		sys.exit(0)
 	
-	# load stored tokens
-	if os.path.exists('tokens.json'):
-		token_json = ''
-		with open('tokens.json') as handle:
-			token_json = handle.read()
-		KNOWN_TOKENS = json.loads(token_json)
-	
 	# init our client
 	smart = SMARTClient(APP_ID, API_BASE, OAUTH_PARAMS)
-	smart.record_id = '1288992'
-	known_token = KNOWN_TOKENS.get(API_BASE, {}).get(smart.record_id)
 	
-	# request a token for a specific record id
-	if known_token is None:
-		smart.fetch_request_token()
-		print 'Now visit:  %s' % smart.auth_redirect_url
-		Popen(['open', smart.auth_redirect_url])
-		oauth_verifier = raw_input('Enter the oauth_verifier: ')
-		
-		# exchange and store the token
-		token = smart.exchange_token(oauth_verifier)
-		if token and token.get('oauth_token') and token.get('oauth_token_secret'):
-			if KNOWN_TOKENS.get(API_BASE) is None:
-				KNOWN_TOKENS[API_BASE] = {}
-			KNOWN_TOKENS[API_BASE][smart.record_id] = token
-			with open('tokens.json', 'w') as handle:
-				handle.write(json.dumps(KNOWN_TOKENS))
-	else:
-		print '->  Reusing existing access token'
-		smart.update_token(known_token)
-	
-	# create a patient and test it against our rules
-	patient = TestRecord(smart)
-	print '->  Patient', patient
-	for rule in rules:
-		print '-->  Testing against', rule
-		rule.match_against(patient)
+	# loop over all records and test against our rules
+	for record_id in smart.loop_over_records():
+		record = TestRecord(smart)
+		print '->  Record', record
+		for rule in rules:
+			print '-->  Testing against', rule
+			rule.match_against(record)
 	
 	
